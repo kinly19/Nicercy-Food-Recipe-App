802a115281543b7734a31d07aedb91ce84a440ca
unknown <kinchoily@hotmail.com>
2021-12-17 20:41:29 +0000

    Create 'Home' page component/styling

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Landing from '../component/Landing/Landing';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <main className="home">
+      <Landing />
+    </main>
+  );
+};
+
+export default Home;
