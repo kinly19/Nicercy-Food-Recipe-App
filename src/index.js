2932932c47770aa5a3f8b1b5f76df6639b2cdc14
unknown <kinchoily@hotmail.com>
2021-12-17 20:58:24 +0000

    index.js - Use AuthContextProvider component as wrapper to provide React context api 'AuthContext'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './store/auth-context';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
