519aee744b6048e33828ff9bc7162e22e63b2b5c
unknown <kinchoily@hotmail.com>
2021-12-22 17:27:02 +0000

    AuthenticateForm.js - Update form to use a custom hook 'useInput' for handling user feedback

--- src/component/Authenticate/AuthenticateForm.js
+++ src/component/Authenticate/AuthenticateForm.js
@@ -1,5 +1,6 @@
 import { useState, Fragment, useContext, useRef } from 'react';
 import AuthContext from '../../store/auth-context'; // React context
+import useInput from '../../hooks/use-inputs'; // custom hook
 import './AuthenticateForm.scss';
 
 const AuthenticateForm = (props) => {
@@ -8,11 +9,48 @@ const AuthenticateForm = (props) => {
   const authCtx = useContext(AuthContext); 
   const {REACT_APP_APIKEY} = process.env;
   
-  // store user inputs
-  const firstNameInputRef = useRef();
-  const lastNameInputRef = useRef();
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef();
+  // custom hook 
+  const {
+    inputValue: enteredFirstName, // store user input
+    inputError: inputNameHasError,
+    blurHandler: nameBlurHandler,
+    inputChangeHandler: nameChangeHandler,
+  } = useInput((inputValue) => inputValue.trim() !== ""); // validate input 
+
+  const {
+    inputValue: enteredLastName,
+    inputError: enteredLastNameHasError,
+    blurHandler: lastNameBlurHandler,
+    inputChangeHandler: lastNameChangeHandler,
+  } = useInput((inputValue) => inputValue.trim() !== "");
+
+  const {
+    inputValue: enteredEmail,
+    inputError: enteredEmailHasError,
+    blurHandler: emailBlurHandler,
+    inputChangeHandler: emailChangeHandler,
+  } = useInput((inputValue) => inputValue.includes("@"));
+
+  const {
+    inputValue: enteredPassword,
+    inputError: enteredPasswordHasError,
+    blurHandler: passwordBlurHandler,
+    inputChangeHandler: passwordChangeHandler,
+  } = useInput((inputValue) => inputValue.length >= 6);
+
+  // style classes for errors
+  const nameInputClass = inputNameHasError
+    ? "form__input form__input--invalid"
+    : "form__input";
+  const lastnameInputClass = enteredLastNameHasError
+    ? "form__input form__input--invalid"
+    : "form__input";
+  const emailInputClass = enteredEmailHasError
+    ? "form__input form__input--invalid"
+    : "form__input";
+  const passwordInputClass = enteredPasswordHasError
+    ? "form__input form__input--invalid"
+    : "form__input";
 
   //handlers
   const showSignUpHandler = () => {
@@ -67,28 +105,58 @@ const AuthenticateForm = (props) => {
   };
 
   return (
+    <Fragment>
+    {error && <ErrorModal errorMessage={errorMessage}/>}
     <div className="form" onSubmit={formSubmitHandler}>
       <h1>{isLoggedIn ? "Login" : "Sign Up"}</h1>
       <form className="form__inputItems">
         {!isLoggedIn && (
           <Fragment>
-            <div className="form__input">
+            <div className={nameInputClass}>
               <label htmlFor="text">First Name</label>
-              <input type="text" id="firstname" required ref={firstNameInputRef}/>
+              <input
+                type="text"
+                id="firstname"
+                required
+                value={enteredFirstName}
+                onBlur={nameBlurHandler}
+                onChange={nameChangeHandler}
+              />
             </div>
-            <div className="form__input">
+            <div className={lastnameInputClass}>
               <label htmlFor="text">Last Name</label>
-              <input type="text" id="lastname" required ref={lastNameInputRef}/>
+              <input
+                type="text"
+                id="lastname"
+                required
+                value={enteredLastName}
+                onBlur={lastNameBlurHandler}
+                onChange={lastNameChangeHandler}
+              />
             </div>
           </Fragment>
         )}
-        <div className="form__input">
+        <div className={emailInputClass}>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" required ref={emailInputRef}/>
+          <input
+            type="email"
+            id="email"
+            required
+            value={enteredEmail}
+            onBlur={emailBlurHandler}
+            onChange={emailChangeHandler}
+          />
         </div>
         <div className="form__input">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" required ref={passwordInputRef}/>
+          <input
+            type="password"
+            id="password"
+            required
+            value={enteredPassword}
+            onBlur={passwordBlurHandler}
+            onChange={passwordChangeHandler}
+          />
         </div>
         <div className="form__btn">
           {isLoggedIn && (
@@ -107,6 +175,7 @@ const AuthenticateForm = (props) => {
         </div>
       </form>
     </div>
+    </Fragment>
   );
 };
 
