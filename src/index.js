266fd52ae72a95b47a6eb06f55456198b4c08356
unknown <kinchoily@hotmail.com>
2022-01-04 18:12:29 +0000

    Import 'RecipeContextProvider' component for providing context for 'RecipeContext'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './store/auth-context';
+import { AuthContextProvider} from './store/auth-context';
+import { RecipeContextProvider } from './store/recipe-context';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RecipeContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecipeContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
