a14f78dfd27e857043d351fd0d1345c7d4eca97d
unknown <kinchoily@hotmail.com>
2022-01-19 19:22:42 +0000

    Create 'AuthPage' component page

---
+++ src/pages/AuthPage.js
@@ -0,0 +1,12 @@
+import AuthenticateForm from "../component/Authenticate/AuthenticateForm";
+import Layout from "../component/Layout/Layout";
+
+const AuthPage = () => { 
+  return (
+    <Layout>
+       <AuthenticateForm />
+    </Layout>
+  );
+};
+
+export default AuthPage;
