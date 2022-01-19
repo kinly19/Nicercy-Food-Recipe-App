784920ef964824fb22b6f3b090a420a0f5c121be
unknown <kinchoily@hotmail.com>
2022-01-19 19:06:09 +0000

    Create 'RecipeInfo' component

---
+++ src/component/Recipe/RecipeInfo.js
@@ -0,0 +1,87 @@
+import { useContext, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Fragment } from 'react/cjs/react.development';
+import useFetch from '../../hooks/useFetch';
+import RecipeContext from '../../store/recipe-context';
+import Ingredients from './Ingredients';
+import Methods from './Methods';
+import './RecipeInfo.scss';
+
+const RecipeInfo = () => {
+
+  const recipeCtx = useContext(RecipeContext);
+  const params = useParams(); // .recipeId
+  const { REACT_APP_SPOONACULARKEY } = process.env;
+
+  const { data: fetchedData } = useFetch(
+    `https://api.spoonacular.com/recipes/${params.recipeId}/information?apiKey=${REACT_APP_SPOONACULARKEY}`,
+    !recipeCtx.recipeItems
+  );
+
+  let selectedRecipe;
+  if (recipeCtx.recipeItems) {
+    const recipeList = recipeCtx.recipeItems.filter((item) => {
+      if (item.id.toString() === params.recipeId) {
+        console.log("recipe Fetched from context");
+        return item;
+      }
+    });
+
+    const similarRecipeList = recipeCtx.similarRecipeItems.filter((item) => {
+      if (item.id.toString() === params.recipeId){
+        console.log("similar recipe Fetched from context");
+        return item
+      }
+    })
+
+    if (recipeList.length > 0) {
+      selectedRecipe = recipeList;
+    } else {
+      selectedRecipe = similarRecipeList;
+    }
+  }
+
+  if (fetchedData) {
+    selectedRecipe = [
+      {
+        id: fetchedData.id,
+        title: fetchedData.title,
+        image: fetchedData.image,
+        readyIn: fetchedData.readyInMinutes,
+        servings: fetchedData.servings,
+        ingredients: fetchedData.extendedIngredients,
+        instructions: fetchedData.analyzedInstructions,
+      },
+    ];
+    console.log("Data fetched from api call");
+  }
+
+  return (
+    <section className="recipeInfo">
+      {!selectedRecipe && !fetchedData && (
+        <div className="recipeInfo__noContent">
+          <h1>No Recipe items found, please try again...</h1>
+          <Link to={"/"}>Return To Home</Link>
+        </div>
+      )}
+      {selectedRecipe && (
+        <Fragment>
+          <div className="recipeInfo__image">
+            <img src={selectedRecipe[0].image} alt={selectedRecipe[0].title} />
+            <div className="recipeInfo__title">
+              <h1>{selectedRecipe[0].title}</h1>
+              <h3>Ready in: {selectedRecipe[0].readyIn} min</h3>
+              <h3>Servings: {selectedRecipe[0].servings}</h3>
+            </div>
+          </div>
+          <div className="recipeInfo__content">
+            <Ingredients data={selectedRecipe} />
+            <Methods data={selectedRecipe} />
+          </div>
+        </Fragment>
+      )}
+    </section>
+  );
+};
+
+export default RecipeInfo;
