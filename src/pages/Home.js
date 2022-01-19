2d4ba4da4ef10ede34980266d7aeb984dd413dc2
unknown <kinchoily@hotmail.com>
2022-01-19 19:17:28 +0000

    Home.js - Use context 'RecipeContext' for conditional rendering of 'RecipeList' component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,29 @@
+import { Fragment, useContext } from 'react';
 import Landing from '../component/Landing/Landing';
+import RecipeList from '../component/Recipe/RecipeList';
+import RecipeContext from '../store/recipe-context';
 import './Home.scss';
 
 const Home = () => {
+  const recipeCtx = useContext(RecipeContext);
+
   return (
     <main className="home">
       <Landing />
+      {recipeCtx.recipeItems && recipeCtx.similarRecipeItems && (
+        <Fragment>
+          <RecipeList
+            title={"Recipes For"}
+            subTitle={recipeCtx.searchInputTitle}
+            data={recipeCtx.recipeItems}
+          />
+          <RecipeList
+            title={"Similar Recipes For"}
+            subTitle={recipeCtx.searchInputTitle}
+            data={recipeCtx.similarRecipeItems}
+          />
+        </Fragment>
+      )}
     </main>
   );
 };
