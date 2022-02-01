a1d7411e5ea8cfe33051a12b9b900268d1115ef5
unknown <kinchoily@hotmail.com>
2022-02-01 23:59:19 +0000

    RecipeItem.js - Add state and logic for 'bookmarkHandler'

--- src/component/Recipe/RecipeItems.js
+++ src/component/Recipe/RecipeItems.js
@@ -1,14 +1,66 @@
+import { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { auth } from '../../Firebase';
+import { BsFillBookmarkHeartFill } from 'react-icons/bs';
+import FavouriteContext from '../../store/favourite-context';
 import "./RecipeItems.scss";
 
 const RecipeItems = (props) => {
 
+  // States
+  const [isFavourite, setIsFavourite] = useState(null);
+  const currentUser = auth.currentUser?.uid;
+  const favouriteList = favouriteCtx.favouriteList.filter((item) => {
+    if (item.Id === props.id) {
+      return item
+    }
+  })
+  const hasItem = favouriteList.length > 0;
+
+  // Context
+  const favouriteCtx = useContext(FavouriteContext);
+
+  // Hook
   let navigate = useNavigate();
 
+  // Handlers
   const navigateHandler = () => {
     navigate(`/recipe-details/${props.id}`);
   };
 
+  const bookmarkHandler = () => {
+    const favouriteItem = {
+      title: props.title,
+      id: props.id,
+      image: props.backgroundUrl,
+    };
+
+    if (currentUser) {
+      if (!hasItem) {
+        setIsFavourite(true);
+        favouriteCtx.addItem(currentUser, favouriteItem);
+        console.log("add item to favourite list");
+      } else {
+        setIsFavourite(false);
+        favouriteCtx.removeItem(currentUser, favouriteItem.id);
+        console.log("remove item from favourite list");
+      }
+    } else {
+      navigate(`/auth`);
+      console.log("please log in!");
+    }
+  };
+
+  useEffect(() => {
+    favouriteCtx.getFavouriteList(currentUser);
+    setIsFavourite(hasItem);
+    if (hasItem) {
+      setIsFavourite(true)
+    } else {
+      setIsFavourite(false)
+    }
+  }, [isFavourite, hasItem]);
+  
   return (
     <li onClick={navigateHandler}>
       <div
