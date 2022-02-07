25a8a056b1165c418134a97e1956bca77e1538ce
unknown <kinchoily@hotmail.com>
2022-02-07 12:02:20 +0000

    RecipeItems.js - Move states/bookmarkHandler and side effects code for handling bookmarked items into separate component (Bookmark.js)

--- src/component/Recipe/RecipeItems.js
+++ src/component/Recipe/RecipeItems.js
@@ -1,62 +1,22 @@
-import { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { auth } from '../../Firebase';
-import { BsFillBookmarkHeartFill } from 'react-icons/bs';
-import FavouriteContext from '../../store/favourite-context';
+import Bookmark from '../UI/Bookmark';
 import "./RecipeItems.scss";
 
 const RecipeItems = (props) => {
-  // Context
-  const favouriteCtx = useContext(FavouriteContext);
+ 
   // Hook
   let navigate = useNavigate();
-  // States
-  const [isFavourite, setIsFavourite] = useState(null);
-  const currentUser = auth.currentUser?.uid;
-  const favouriteList = favouriteCtx.favouriteList.filter((item) => {
-    if (item.Id === props.id) {
-      return item;
-    }
-  });
-  const hasItem = favouriteList.length > 0;
 
+  const favouriteItem = {
+    title: props.title,
+    id: props.id,
+    image: props.backgroundUrl,
+  };
+  
   // Handlers
   const navigateHandler = () => {
     navigate(`/recipe-details/${props.id}`);
   };
-
-  const bookmarkHandler = () => {
-    const favouriteItem = {
-      title: props.title,
-      id: props.id,
-      image: props.backgroundUrl,
-    };
-
-    if (currentUser && !hasItem) {
-      setIsFavourite(true);
-      favouriteCtx.addItem(currentUser, favouriteItem);
-      console.log("add item to favourite list");
-    } else {
-      setIsFavourite(false);
-      favouriteCtx.removeItem(currentUser, favouriteItem.id);
-      console.log("remove item from favourite list");
-    }
-
-    if (!currentUser) {
-      navigate(`/auth`);
-      console.log("please log in!");
-    }
-  };
-
-  useEffect(() => {
-    favouriteCtx.getFavouriteList(currentUser);
-    setIsFavourite(hasItem);
-    if (hasItem) {
-      setIsFavourite(true);
-    } else {
-      setIsFavourite(false);
-    }
-  }, [isFavourite, hasItem]);
 
   return (
     <li onClick={navigateHandler}>
