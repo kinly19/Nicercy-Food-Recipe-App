ac31b2483216f6625dfe1b3f4a0f675b02bc0017
unknown <kinchoily@hotmail.com>
2022-02-08 19:59:22 +0000

    Create 'Favourite' component for Favourites page

---
+++ src/pages/Favourite.js
@@ -0,0 +1,13 @@
+import FavouriteList from "../component/Favourite/FavouriteList";
+import Layout from "../component/Layout/Layout";
+import "./Favourite.scss";
+
+const Favourite = () => {
+  return ( 
+    <Layout>
+      <FavouriteList />
+    </Layout>
+  );
+};
+
+export default Favourite;
