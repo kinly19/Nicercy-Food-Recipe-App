ad23a59ff50ba09fb8f1fd25f29644d9d031ce7e
unknown <kinchoily@hotmail.com>
2022-02-10 18:39:26 +0000

    RecipeInfo.js - Update/Fix component code, add side effect for handling 'hasNoRecipe' state if no matching recipe id is found. Pass updated state as an argument to fetch hook

--- src/component/Recipe/RecipeInfo.js
+++ src/component/Recipe/RecipeInfo.js
@@ -1,6 +1,5 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { Fragment } from 'react/cjs/react.development';
 import useFetch from '../../hooks/useFetch';
 import RecipeContext from '../../store/recipe-context';
 import Bookmark from '../UI/Bookmark';
@@ -14,33 +13,35 @@ const RecipeInfo = () => {
   const recipeCtx = useContext(RecipeContext);
   const params = useParams(); // .recipeId
   const { REACT_APP_SPOONACULARKEY } = process.env;
+  
+  const [hasNoRecipe, setHasNoRecipe] = useState(null); 
+  let selectedRecipe;
 
   const { data: fetchedData, isLoading: loading } = useFetch(
     `https://api.spoonacular.com/recipes/${params.recipeId}/information?apiKey=${REACT_APP_SPOONACULARKEY}`,
-    !recipeCtx.recipeItems
+    hasNoRecipe
   );
 
-  let selectedRecipe;
-  if (recipeCtx.recipeItems) {
-    const recipeList = recipeCtx.recipeItems.filter((item) => {
-      if (item.id.toString() === params.recipeId) {
-        console.log("recipe Fetched from context");
-        return item;
-      }
-    });
-
-    const similarRecipeList = recipeCtx.similarRecipeItems.filter((item) => {
-      if (item.id.toString() === params.recipeId){
-        console.log("similar recipe Fetched from context");
-        return item
-      }
-    })
-
-    if (recipeList.length > 0) {
-      selectedRecipe = recipeList;
-    } else {
-      selectedRecipe = similarRecipeList;
+  const recipeList = recipeCtx.recipeItems?.filter((item) => {
+    if (item.id.toString() === params.recipeId) {
+      console.log("recipe Fetched from context");
+      return item;
     }
+  });
+
+  const similarRecipeList = recipeCtx.similarRecipeItems?.filter((item) => {
+    if (item.id.toString() === params.recipeId) {
+      console.log("similar recipe Fetched from context");
+      return item;
+    }
+  });
+
+  if (recipeList?.length > 0) {
+    selectedRecipe = recipeList;
+  }
+
+  if (similarRecipeList?.length > 0) {
+    selectedRecipe = similarRecipeList;
   }
 
   if (fetchedData) {
@@ -55,8 +56,16 @@ const RecipeInfo = () => {
         instructions: fetchedData.analyzedInstructions,
       },
     ];
-    console.log("Data fetched from api call");
   }
+
+  useEffect(() => {
+    if (!selectedRecipe) {
+      setHasNoRecipe(true);
+      console.log("Data fetched from api");
+    }
+  }, [selectedRecipe]);
+
+  const { image, title, id, readyIn, servings } = selectedRecipe?.[0] || {};
 
   const showNoContent = (
     <div className="recipeInfo__noContent">
