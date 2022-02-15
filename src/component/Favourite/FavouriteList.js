55416fc16b815f9c4c97f51d296f7d6bfed8dbaa
unknown <kinchoily@hotmail.com>
2022-02-15 14:05:50 +0000

    FavouriteList.js - Add conditional content to clean up jsx

--- src/component/Favourite/FavouriteList.js
+++ src/component/Favourite/FavouriteList.js
@@ -25,19 +25,29 @@ const FavouriteList = () => {
     favouriteCtx.getFavouriteList();
   }, [currentUser]);
 
+  // Conditional Content
+  const showLoading = favouriteCtx.isLoading && <Loading align={"center"}/>;
+
+  let favouritesContent = <h2>No favourites found...</h2>;
+
+  if (favouriteCtx.favouriteList.length > 0 && !favouriteCtx.isLoading) {
+    favouritesContent = favouriteCtx.favouriteList.map((item) => (
+      <FavouriteItem
+        key={item.Id}
+        title={item.Title}
+        onRemove={() => onRemoveHandler(item.Id)}
+        onNavigate={() => onNavigateHandler(item.Id)}
+      />
+    ));
+  }
+
   return (
     <div className="favourite">
       <h1 className="favourite__title">Favourite</h1>
       <p className="favourite__subTitle">Recipes</p>
       <ul className="favourite__list">
-        {favouriteCtx.favouriteList.map((item) => (
-          <FavouriteItem
-            key={item.Id}
-            title={item.Title}
-            onRemove={() => onRemoveHandler(item.Id)}
-            onNavigate={() => onNavigateHandler(item.Id)}
-          />
-        ))}
+        {showLoading}
+        {favouritesContent}
       </ul>
     </div>
   );
