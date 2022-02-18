7a02ce10c5366965180fd04eabbf004ea93cc6d5
unknown <kinchoily@hotmail.com>
2022-02-18 21:23:22 +0000

    App.js - Add 'MainNavigation' component, update and add react routes for additional pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate} from 'react-router-dom';
+import { Fragment, useContext } from 'react';
 import './App.scss';
-import Layout from './component/UI/Layout';
+import AuthContext from './store/auth-context';
+import Home from './pages/Home';
+import MainNavigation from './component/Layout/MainNavigation';
+import Favourite from './pages/Favourite';
+import AuthPage from './pages/AuthPage';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
+  const authCtx = useContext(AuthContext);
+  
   return (
-    <Layout>
+    <Fragment>
+      <MainNavigation />
       <Routes>
-        <Route path="/"/> 
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthPage />}/>
+        <Route path="/recipe-details" element={<RecipeDetails />}>
+          <Route path=":recipeId" element={<RecipeDetails />} />
+        </Route>
+        {/* Protected routes */}
+          {authCtx.isLoggedIn && <Route path="/favourite" element={<Favourite />} />}
+          {!authCtx.isLoggedIn && <Route path="/favourite" element={<Navigate to="/auth" />}/>}
       </Routes>
-    </Layout>
+    </Fragment>
   );
 }
-
 export default App;
