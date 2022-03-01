7dc2d272100ca5ddc0cc8872279af88257a7b91d
unknown <kinchoily@hotmail.com>
2022-03-01 11:34:37 +0000

    Landing.js - Fix side effect to scroll page down to the end of 'landing' container

--- src/component/Landing/Landing.js
+++ src/component/Landing/Landing.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, Fragment } from 'react';
+import { useContext, useEffect, Fragment, useRef} from 'react';
 import RecipeForm from '../RecipeForm/RecipeForm';
 import Loading from '../UI/Loading';
 import ErrorModal from '../UI/ErrorModal';
@@ -7,13 +7,14 @@ import './Landing.scss';
 
 const Landing = () => {
   const recipeCtx = useContext(RecipeContext);
+  const containerHeight = useRef();
   const showLoading = recipeCtx.Loading && <Loading align={"flex-start"}/>
   const showError = recipeCtx.error && <ErrorModal errorMessage={recipeCtx.errorMessage} />
-
+ 
   useEffect(() => {
     if (recipeCtx.recipeItems || recipeCtx.similarRecipeItems) {
       window.scroll({
-        top: 1000,
+        top: containerHeight.current.clientHeight - 50,
         behavior: 'smooth'
       });
     }
@@ -22,7 +23,7 @@ const Landing = () => {
   return (
     <Fragment>
       {showError}
-      <section className="landing">
+      <section className="landing" ref={containerHeight}>
         <div className="landing__content">
           <div className="landing__header">
             <h1>Nicercy</h1>
