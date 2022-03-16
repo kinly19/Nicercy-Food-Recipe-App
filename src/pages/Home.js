ac0f4ee531225a1ceef75e6296103b6e7940ab21
unknown <kinchoily@hotmail.com>
2022-03-16 16:37:49 +0000

    Home.js - Add side effect to conditionally scroll page back to top

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import Landing from '../component/Landing/Landing';
 import RecipeList from '../component/Recipe/RecipeList';
 import RecipeContext from '../store/recipe-context';
@@ -6,6 +6,12 @@ import './Home.scss';
 
 const Home = () => {
   const recipeCtx = useContext(RecipeContext);
+
+  useEffect(() => {
+    if (!recipeCtx.recipeItems){
+      window.scrollTo(0,0);
+    }
+  },[])
 
   return (
     <main className="home">
