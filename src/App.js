33df36e7785c1bcd0e39b0add3721d258797ed7d
unknown <kinchoily@hotmail.com>
2022-03-21 13:31:35 +0000

    App.js - Add side effect to handle page scroll to top

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Route, Routes, Navigate} from 'react-router-dom';
-import { Fragment, useContext } from 'react';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { Fragment, useContext, useEffect } from 'react';
 import './App.scss';
 import AuthContext from './store/auth-context';
+import RecipeContext from './store/recipe-context';
 import Home from './pages/Home';
 import MainNavigation from './component/Layout/MainNavigation';
 import Favourite from './pages/Favourite';
@@ -11,6 +12,17 @@ import Footer from './component/Layout/Footer';
 
 function App() {
   const authCtx = useContext(AuthContext);
+  const recipeCtx = useContext(RecipeContext);
+  
+  const pathName = useLocation();
+  const isPathHome = pathName.pathname === '/';
+
+  useEffect(() => {
+    // scroll page to top
+    if ((!recipeCtx.recipeItems && isPathHome) || !isPathHome) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathName]);
   
   return (
     <Fragment>
