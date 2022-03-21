ccabfe205c1192c22391b3e246e0a5081739d08a
unknown <kinchoily@hotmail.com>
2022-03-21 13:28:24 +0000

    Home.js - Remove page scroll to top side effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import Landing from '../component/Landing/Landing';
 import RecipeList from '../component/Recipe/RecipeList';
 import RecipeContext from '../store/recipe-context';
@@ -6,12 +6,6 @@ import './Home.scss';
 
 const Home = () => {
   const recipeCtx = useContext(RecipeContext);
-
-  useEffect(() => {
-    if (!recipeCtx.recipeItems){
-      window.scrollTo(0,0);
-    }
-  },[])
 
   return (
     <main className="home">
